3ed1fa5b0e2adf305bc4a4e1a0862552c43ce5c8
Daniel LoFaso <daniel.lofaso@gmail.com>
2026-04-20 11:42:27 -0400

    update readme

--- models/clothingItem.js
+++ models/clothingItem.js
@@ -25,12 +25,15 @@ const clothingItemSchema = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
     required: true,
   },
-  likes: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
   createdAt: {
     type: Date,
     default: Date.now,
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,14 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
 
+const { NOT_FOUND_STATUS_CODE } = require("../utils/errors");
+
+router.use((req, res) => {
+  res
+    .status(NOT_FOUND_STATUS_CODE)
+    .send({ message: "Requested resource not found" });
+});
+
 router.use("/users", userRouter);
 router.use("/items", clothingItemsRouter);
 
